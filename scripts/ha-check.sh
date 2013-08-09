62d38f1f0c14843f5800d7f9bd69d8094c57b90e
Yazz Atlas <yazz.atlas@hp.com>
2013-08-09 18:39:15 +0000

    adding check for 9200

--- scripts/ha-check.sh
+++ scripts/ha-check.sh
@@ -2,7 +2,7 @@
 # dirty check to see if things are actually HA
 my_ip=$(ip addr show bond0 | grep "inet "| awk '{print $2}')
 cluster="{% for key, args in pillar['endpoints']['rabbit']['servers'].iteritems() %}{{ args }} {% endfor %}"
-ports="5671 3306"
+ports="5671 3306 9200"
 for port in ${ports} ; do
         echo "-------------------------------------------"
         echo "Testing: ${cluster}"
