c43e0f1ea9b09b7f72cbbf93f2ccea33056970ba
Yazz Atlas <yazz.atlas@hp.com>
2013-08-20 01:21:21 +0000

    adding basic backups for our db

---
+++ scripts/db-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+backup_dir="/var/lib/backups/mysql"
+filename="${backup_dir}/mysql-`hostname`-`eval date +%Y%m%d`.sql.gz"
+# Dump the entire MySQL database
+/usr/bin/mysqldump --opt --all-databases | gzip > $filename 
+# Delete backups older than 7 days
+find $backup_dir -ctime +7 -type f -delete
